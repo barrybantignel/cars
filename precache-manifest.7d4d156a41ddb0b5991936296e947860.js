01f19f7120f261e0712be5834a5ee0cf1ca7dd3a
barryboubacar <barrybantignel@gmail.com>
2020-12-01 15:01:03 +0100

    Updates

---
+++ precache-manifest.7d4d156a41ddb0b5991936296e947860.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da39b4f201d84bbeb7f1336c15af62aa",
+    "url": "/cars/index.html"
+  },
+  {
+    "revision": "28b3f2a74785ac6b824d",
+    "url": "/cars/static/css/main.5cadb214.chunk.css"
+  },
+  {
+    "revision": "19ffe96f1e77424d9ebc",
+    "url": "/cars/static/js/2.0724d4c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cars/static/js/2.0724d4c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28b3f2a74785ac6b824d",
+    "url": "/cars/static/js/main.e7056399.chunk.js"
+  },
+  {
+    "revision": "040828672ebb5d2f67a9",
+    "url": "/cars/static/js/runtime-main.d4761892.js"
+  }
+]);
